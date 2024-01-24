21a9dbe386ba5291f3bce02455110fe7f7476787
Nikhila Jain <99252011+jnikhila@users.noreply.github.com>
2024-01-24 21:51:39 +0530

    Added my-docs-website
    Docs-only website

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # learn-docusaurus
+
+The **learn-docusaurus** repository is dedicated to learning and exploring Docusaurus, an open-source project for building, deploying, and maintaining documentation websites. This repository serves as a collection of projects built using Docusaurus, showcasing various use cases and examples that can be helpful for anyone looking to learn or improve their skills in using this powerful documentation framework.
+
+## About Docusaurus
+
+[Docusaurus](https://docusaurus.io) is a powerful documentation framework that makes it easy to maintain and deploy documentation websites.
+
+## Repository Structure
+
+Each project in this repository is a standalone example of a specific use case or feature of Docusaurus. These projects are designed to provide practical insights and hands-on experience with different aspects of Docusaurus.
+
+## Featured Projects
+
+* my-docs-website: An introductory docs-only project to help you get up and running with Docusaurus Docs-only mode.
+* theme-customization: Demonstrates how to customize the look and feel of a Docusaurus site. - Coming Soon
+* versioning-docs: An example of handling documentation versioning with Docusaurus.- Coming Soon
+
+## Getting Started
+
+To get started with Docusaurus learning journey, follow these steps:
+
+* Clone repository
+
+    ```bash
+    git clone https://github.com/jnikhila/learn-docusaurus.git
+    ```
+
+* Go to the desired project directory. For example, my-docs-website
+
+    ```bash
+    cd learn-docusaurus/my-docs-website
+    ```
+
+* Install dependencies
+
+    ```bash
+    npm install
+    ```
+
+* Start the development server
+
+    ```bash
+    npm start
+    ```
+    This command runs the development server and opens up your browser to the `my-docs-website` site.
+
+## Contributing
+
+Contributions to this repository are welcome! If you have a Docusaurus project or an example that you believe would be beneficial to others, please feel free to fork this repository, add your project, and submit a pull request.
+
+## License
+
+This repository is licensed under the BSD 3-Clause License. For more details, see the [LICENSE](/LICENSE) file in the repository.
---
+++ my-docs-website/docusaurus.config.js
@@ -0,0 +1,81 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Learn Docusaurus by Nikhila Jain',
+  tagline: 'Build Docs-only website with Docusaurus',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'jnikhila', // Usually your GitHub org/user name.
+  projectName: 'learn-docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: './sidebars.js',
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        items: [
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} Nikhila Jain, Technical Writer.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
